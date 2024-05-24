2117c8d272a7e4c9789b4403e6a443beecdb25b0
Juan Cheli <juancheli29@gmail.com>
2024-05-23 21:10:52 -0300

    Ventana vacia en interfaz (funcional)

---
+++ Interfaz/interfazImportarActualizacion.py
@@ -0,0 +1,20 @@
+import sys
+from PyQt6.QtWidgets import QApplication, QWidget
+
+class Ventana(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.inicializarUI()
+
+    def inicializarUI(self):
+        self.setGeometry(500, 500, 800, 800)
+        self.setWindowTitle("BonVino - Importar Actualizacion")
+        self.show()
+
+
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ventana = Ventana()
+    sys.exit(app.exec())
