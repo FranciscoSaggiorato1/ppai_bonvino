535d71403513428037a2c8f3e470a569bd2fdda1
Marcos Pomenich <marcospomenich@gmail.com>
2024-05-29 11:30:49 -0300

    Cambios que no van perdon

--- Interfaz/interfazImportarActualizacion.py
+++ Interfaz/interfazImportarActualizacion.py
@@ -39,12 +39,12 @@ class Ventana(QMainWindow):
     
     def cargar_data(self, bodegas):
         row_count = len(bodegas)
-        column_count = 6  # Número de columnas
+        column_count = 2  # Número de columnas
 
         self.tableWidget.setRowCount(row_count)
         self.tableWidget.setColumnCount(column_count)
         self.tableWidget.verticalHeader().hide()
-        headers = ["Acción", "Nombre", "Coordenadas", "Descripción", "Historia", "Periodo de Actualización"]
+        headers = ["Acción", "Nombre"]
         self.tableWidget.setHorizontalHeaderLabels(headers)
 
         for row_index, bodega in enumerate(bodegas):
@@ -52,10 +52,7 @@ class Ventana(QMainWindow):
             button.clicked.connect(partial(self.tomarBodegaSeleccionada, bodega))
             self.tableWidget.setCellWidget(row_index, 0, button)
             self.tableWidget.setItem(row_index, 1, QTableWidgetItem(bodega['nombre']))
-            self.tableWidget.setItem(row_index, 2, QTableWidgetItem(bodega['coordenadasUbicacion']))
-            self.tableWidget.setItem(row_index, 3, QTableWidgetItem(bodega['descripcion']))
-            self.tableWidget.setItem(row_index, 4, QTableWidgetItem(bodega['historia']))
-            self.tableWidget.setItem(row_index, 5, QTableWidgetItem(str(bodega['periodoActualizacion'])))
+
 
     def tomarBodegaSeleccionada(self, bodega):
         print('La bodega desde interfaz es:', bodega)
