d5a30617570eefa432cb78bcbb91c89519dd8d2c
FranciscoSaggiorato1 <94162@sistemas.frc.utn.edu.ar>
2024-11-04 18:06:50 -0300

    Añado nuevos cambios de sujeto notificacion

---
+++ Modelo/ISujetoNotificacionPush.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+
+# Interfaz del Sujeto
+class ISujetoNotificacionPush(ABC):
+
+    def __init__(self):
+        self.observadores = []
+
+    @abstractmethod
+    def suscribir(self, observers: list):
+        pass
+
+    @abstractmethod
+    def quitar(self, observers: list):
+        pass
+
+    @abstractmethod
+    def notificar(self):
+        pass
